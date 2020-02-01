f1e7aede17bbfa72a4eed56cc7bd03f4004b4f69
Payaam Emami <contact@payaame.com>
2020-02-01 00:45:17 -0800

    tslint fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,30 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from "@angular/material/card";
-import { MatDividerModule } from "@angular/material/divider";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatIconModule } from "@angular/material/icon";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { LayoutModule } from "@angular/cdk/layout";
-import { RouterModule, Routes } from "@angular/router";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LayoutModule } from '@angular/cdk/layout';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { FooterComponent } from "./components/footer/footer.component";
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { HomeComponent } from "./components/home/home.component";
-import { HomeHeaderComponent } from "./components/home/home-header/home-header.component";
-import { HomePromoComponent } from "./components/home/home-promo/home-promo.component";
-import { PageHeaderComponent } from "./components/page-header/page-header.component";
-import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomeHeaderComponent } from './components/home/home-header/home-header.component';
+import { HomePromoComponent } from './components/home/home-promo/home-promo.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "**", component: PageNotFoundComponent }
+  { path: '', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
